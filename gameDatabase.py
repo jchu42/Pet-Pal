576a5c396dd3d97e1d77e59f361029e16cef969a
Lea Barnachea <lbarnachea@myseneca.caa>
2024-03-04 19:21:22 -0500

    new database file

---
+++ gameDatabase.py
@@ -0,0 +1,11 @@
+# this file holds the rough code on how to connect python and postgres
+
+#step 1: in a terminal window type: pip3 install psycopg2
+
+#step 2: in postgres account (assumming a new database is already made), 
+#we'll need the following info:
+#hostname  
+#database 
+#pwd
+#port_id
+
